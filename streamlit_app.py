e4ab3645b4bf984ef361b6d13f9be6d677816ac1
josueHakk <143032014+josueHakk@users.noreply.github.com>
2023-09-05 10:08:47 -0600

    Update streamlit_app.py
    Fruityvice section added.

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,3 +20,10 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
+
+#New section to desplay frutyvice API response.
+streamlit.header('Frutyvice Fruit Advice!')
+
+import requests
+frutyvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
